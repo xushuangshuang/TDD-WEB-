c919cec3222342ea86a879a5706635af6671f7fe
Dev--xu <xushuangshuang1@gmail.com>
2014-08-09 01:25:16 +0800

    动态的获取类的方法并且调用

--- src/main/java/com/xu/ManageServlet.java
+++ src/main/java/com/xu/ManageServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.util.regex.Pattern;
+import java.lang.reflect.Method;
 
 public class ManageServlet extends HttpServlet
 {
@@ -18,6 +19,18 @@ public class ManageServlet extends HttpServlet
         String classPart = resolveURI[2];
         resolveURI[2]=resolveURI[2].substring(0,1).toUpperCase()+resolveURI[2].substring(1);
         System.out.println(resolveURI[2]);
+        try
+        {
+            Class clazz = Class.forName("com.xu." + resolveURI[2]);
+            Method listMethod = clazz.getDeclaredMethod("list");
+            System.out.println(listMethod);
+            Object obj = clazz.newInstance();
+            listMethod.invoke(obj);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 
 }
