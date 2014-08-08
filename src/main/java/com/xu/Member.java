f18ba9fd51e15cc5c05c4a5dc1cd0018378da673
Dev--xu <xushuangshuang1@gmail.com>
2014-08-09 01:33:29 +0800

    添加了一个Member工具类

---
+++ src/main/java/com/xu/Member.java
@@ -0,0 +1,55 @@
+package com.xu;
+
+public class Member
+{
+    private String name;
+    private String email;
+    private int age;
+    private String address;
+    private int phone;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setPhone(int phone)
+    {
+        this.phone = phone;
+    }
+    public int getPhone()
+    {
+        return phone;
+    }
+}
